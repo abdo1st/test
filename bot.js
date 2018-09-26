72af237c27fcaa7db81cf3c054b37db6cf75e3e0
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-26 20:12:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -761,7 +761,7 @@ if (message.content === '*invite') {
  
  
 client.on('message', message => {
-     if (message.content === "$invite") {
+     if (message.content === "*invite") {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
